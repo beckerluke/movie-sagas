36040072e3840fc9e74a30e9641933963175c05e
Luke Becker <beckerluke1@gmail.com>
2019-09-06 17:08:59 -0500

    setup DetailsPage component and routed it in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import MovieListPage from '../MovieListPage/MovieListPage';
+import DetailsPage from '../DetailsPage/DetailsPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -13,6 +14,7 @@ class App extends Component {
         </header>
         <Router>
           <Route exact path="/" component={MovieListPage}/>
+          <Route exact path="/details" component={DetailsPage}/>
         </Router>
       </div>
     );
---
+++ src/components/DetailsPage/DetailsPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App/App.css';
+
+class DetailsPage extends Component {
+  // Renders the description and genres for specific movie selected
+  render() {
+    return (
+      <div className="App">
+        <p>Movie Details</p>
+      </div>
+    );
+  }
+}
+
+export default DetailsPage;
