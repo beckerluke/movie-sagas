50487529ecd83815b40a6e2d5cfa8289a235a760
Luke Becker <beckerluke1@gmail.com>
2019-09-06 18:01:56 -0500

    Fixed movie router on server-side. Created GET to pull data from database

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -1,8 +1,28 @@
 const express = require('express');
-
 // will need to get info off database
 const pool = require('../modules/pool');
-
 const router = express.Router();
+
+// GET route to pull all movie data from database and send to client
+router.get('/', (req, res) => {
+    
+    // query database to join movies and genres tables
+    
+    const queryText = `SELECT * FROM "movies"
+    // JOIN "movies_genres" ON "movies".id = "movies_genres".movies_id
+    // JOIN "genres" ON "movies_genres".genres_id = "genres".id
+    // ORDER BY "movies".title ASC;`;
+
+    pool.query(queryText)
+        .then((result) => {
+            console.log('GET from DB ', result);
+            // sends database information back to client in array
+            res.send(result.rows)
+        })
+        .catch((err) => {
+            console.log('ERROR completing movie query ', err);
+            res.sendStatus(500);
+        });
+}); // End of GET 
 
 module.exports = router;
