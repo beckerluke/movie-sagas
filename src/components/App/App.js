a6779ce56ce0a6e266fe93b1c25e8581a5c11a52
Luke Becker <beckerluke1@gmail.com>
2019-09-06 17:23:43 -0500

    Created EditPage Component and routed it in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import MovieListPage from '../MovieListPage/MovieListPage';
 import DetailsPage from '../DetailsPage/DetailsPage';
+import EditPage from '../EditPage/EditPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -15,6 +16,7 @@ class App extends Component {
         <Router>
           <Route exact path="/" component={MovieListPage}/>
           <Route exact path="/details" component={DetailsPage}/>
+          <Route exact path="/edit" component={EditPage}/>
         </Router>
       </div>
     );
---
+++ src/components/EditPage/EditPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App/App.css';
+
+class EditPage extends Component {
+  // Renders input field and textarea for user to edit the title and description for specific movie selected
+  render() {
+    return (
+      <div className="App">
+        <p>Edit Movie</p>
+      </div>
+    );
+  }
+}
+
+export default EditPage;
