14e267bf87ade1a33339b44e84b89b4af5e344a3
Luke Becker <beckerluke1@gmail.com>
2019-09-06 17:02:07 -0500

    setup router in App and created MovieListPage component and routed to it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import MovieListPage from '../MovieListPage/MovieListPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <header className="App-header">
+          <h1>MOVIE WORLD</h1>
+        </header>
+        <Router>
+          <Route exact path="/" component={MovieListPage}/>
+        </Router>
       </div>
     );
   }
---
+++ src/components/MovieListPage/MovieListPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App/App.css';
+
+class MovieListPage extends Component {
+  // Renders the entire list of movies and posters from Database
+  render() {
+    return (
+      <div className="App">
+        <p>Movie List Page</p>
+      </div>
+    );
+  }
+}
+
+export default MovieListPage;
