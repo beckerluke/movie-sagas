b1d7cfb1b032308602f107dcce9d5219abf7c720
Luke Becker <beckerluke1@gmail.com>
2019-09-06 17:39:45 -0500

    Created movie route in server

---
+++ server/routes/movie.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// will need to get info off database
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+// Route includes
+const movieRouter = require('./routes/movie.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/movie', movieRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
